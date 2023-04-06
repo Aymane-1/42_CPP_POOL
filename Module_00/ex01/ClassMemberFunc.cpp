2fb2623ffdddc3e1879cbe77f1854742bcd5c4d7
Aymane Echafii <echafii.aymane@gmail.com>
2023-04-06 18:34:52 +0000

    -add: define getters and setters for each function attributes

---
+++ Module_00/ex01/ClassMemberFunc.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClassMemberFunc.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 02:45:37 by aechafii          #+#    #+#             */
+/*   Updated: 2023/04/06 18:33:11 by aechafii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+//-----------------------*** Contact Class member functions ***-----------------------//
+
+void	Contact::get_FirstName(void)
+{
+		std::cout << "here" << Contact::FirstName;
+}
+
+void	Contact::get_LastName(void)
+{
+		std::cout << Contact::LastName;
+}
+
+void	Contact::get_NickName(void)
+{
+		std::cout << Contact::NickName;
+}
+
+void	Contact::get_PhoneNumber(void)
+{
+		std::cout << Contact::PhoneNumber;
+}
+
+void	Contact::get_DarkestSecret(void)
+{
+		std::cout << Contact::DarkestSecret;
+}
+
+
+void	Contact::set_FirstName(std::string s)
+{
+		Contact::FirstName = s;
+}
+
+void	Contact::set_LastName(std::string s)
+{
+		Contact::LastName = s;
+}
+
+void	Contact::set_NickName(std::string s)
+{
+		Contact::NickName = s;
+}
+
+void	Contact::set_PhoneNumber(std::string s)
+{
+		Contact::PhoneNumber = s;
+}
+
+void	Contact::set_DarkestSecret(std::string s)
+{
+		Contact::DarkestSecret = s;
+}
+
+//-----------------------*** PhoneBook Class member functions ***-----------------------//
+
