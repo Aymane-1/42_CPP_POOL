5790279b98be6380f7e23066c87ca9b30ea65c96
Aymane Echafii <echafii.aymane@gmail.com>
2023-06-02 23:24:41 +0100

    ex00 Complete

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 21:36:22 by aechafii          #+#    #+#             */
-/*   Updated: 2023/06/02 21:45:58 by aechafii         ###   ########.fr       */
+/*   Updated: 2023/06/02 23:03:07 by aechafii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 #define EASYFIND_HPP
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-template <typename T>
-void	easyfind(T data, int num)
+template <typename containerT>
+void	easyfind(containerT data, int num)
 {
-	
+	std::vector<int>::iterator it;
+	it = std::find(data.begin(), data.end(), num);
+	if (it != data.end())		
+		std::cout << "Element found." << std::endl;
+	else
+		std::cout << "Element not found!" << std::endl;
 }
 
 #endif
