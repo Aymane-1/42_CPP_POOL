b8a36eb0ff92602b9b333cf9d52b4349796d0f8a
Aymane Echafii <echafii.aymane@gmail.com>
2023-06-02 21:47:51 +0100

    -add: Initial commit / Creation of files

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 21:36:22 by aechafii          #+#    #+#             */
+/*   Updated: 2023/06/02 21:45:58 by aechafii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+void	easyfind(T data, int num)
+{
+	
+}
+
+#endif
